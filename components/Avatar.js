620c89a5c154faf4009e9eb9af6b4ccbb5b9d8a5
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2023-12-07 12:41:10 +0600

    home page design complete

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div>
+    <Image src={'/Avater.my.png'}  alt="avatar" width={600} height={600}/>
+  </div>;
 };
 
 export default Avatar;
--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 const Header = () => {
 
-  return <header className=" text-white">Header</header>;
+  return <header className=" absolute z-30 w-full h-full bg-explosion xl:bg-no-repeat bg-cover mix-blend-overlay"></header>;
 };
 
 export default Header;
